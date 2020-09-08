99932f372ccaf48cfb24a8651c73f052f78a23c8
Yomudogly <khalnepesov@gmail.com>
2020-09-07 23:04:06 -0400

    fix reply for private messages

--- src/app.js
+++ src/app.js
@@ -100,8 +100,9 @@ client.on("message", (msg) => {
         msg.delete({ timeout: 7000 });
       });
 
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-  if (!msg.guild) {
+  if (!msg.content.startsWith(prefix) || msg.author.bot) {
+    return;
+  } else if (!msg.guild || (!msg.guild && !msg.content.startsWith(prefix))) {
     return msg.reply(
       "Personal messages not permitted :face_with_raised_eyebrow: If you want me to translate something send message in the server :v:"
     );
